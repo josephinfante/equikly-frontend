45c264a3add304d8d0a67f897a428e233d4b48ff
Joseph Infante <josephinfante.y@gmail.com>
2024-02-27 15:12:33 -0500

    adding colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#663FAB',
+        yellow: '#FFD700',
+        'light-gray': '#595959',
+        'dark-gray': '#404040'
       },
     },
   },
